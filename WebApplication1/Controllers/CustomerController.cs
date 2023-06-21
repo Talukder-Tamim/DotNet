4175b1ec3028885b7fce9f43e752d08ea0d2c878
Talukder Tamim <md.talukdertamim@gmail.com>
2023-06-21 14:17:33 +0600

    first dot net app

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string List()
+        {
+            return "This is a customer list..";
+        }
+
+        public string Create(CustomerCreate customer)
+        {
+            return $"Create customer, Name: {customer.Name}, Phone:{customer.Phone}, Email: {customer.Email}";
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomerCreate.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class CustomerCreate
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }   
+        
+    }
+}
