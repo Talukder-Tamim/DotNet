25744d389e15aa1fd393a9220721a6bde770ae09
Talukder Tamim <md.talukdertamim@gmail.com>
2023-10-02 18:24:40 +0600

    api response

--- Ecommerce.API/Controllers/CustomersController.cs
+++ Ecommerce.API/Controllers/CustomersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/customers")]
     [ApiController]
     public class CustomersController : ControllerBase
     {
@@ -20,6 +20,32 @@ namespace Ecommerce.API.Controllers
         public IActionResult Get()
         {
             var customers = _customerService.GetAll();
+            if(customers == null)
+            {
+                var responseObj = new 
+                {
+                    statusCode = 200,
+                    errorMessage = "Not found"
+                };
+                return NotFound(responseObj);
+            }
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        { 
+            var customers = _customerService.GetByID(id);
+
+            if(customers == null)
+            {
+                var responseObj = new
+                {
+                    statusCode = 200,
+                    errorMessage = "Not found"
+                };
+                return NotFound(responseObj);
+            }
             return Ok(customers);
         }
     }
